66ecc7a1cb011d0c7775de8f815a26477a13cf5d
Cédric Verstraeten <cedric@verstraeten.io>
2026-03-31 19:05:06 +0000

    feat: add favicon links to Storybook manager head and remove brandImage from config

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -7,7 +7,6 @@ addons.setConfig({
     fontBase: 'Outfit, sans-serif',
     fontCode: 'Inter, sans-serif',
     brandTitle: 'UUG',
-    brandImage: 'favicon-32x32.png',
     brandUrl: '/components',
     brandTarget: '_self',
     colorSecondary: '#ffffff',
